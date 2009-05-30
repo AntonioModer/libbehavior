7ecfa4c6333c6270e587e39ce5f5eb98ace2cceb
LeegleechN <LeegleechN@022deeea-31cc-11de-96ab-f16d9912eac9>
2009-05-30 03:58:21 +0000

    Added a cooldown node

---
+++ trunk/Snow Leopard/Snow Leopard/Cooldown.h
@@ -0,0 +1,29 @@
+#ifndef COOLDOWN_H_
+#define COOLDOWN_H_
+#include "BehaviorTree.h"
+
+using namespace BehaviorTree;
+namespace SL
+{
+	namespace Behaviors
+	{
+	/// Do nothing for the specified amount of time, then return success
+	class Cooldown:public BehaviorTree::BehaviorTreeNode
+	{
+	public:
+		int limit, current;
+		BehaviorTree::BEHAVIOR_STATUS execute(void* agent);
+		void init(void* agent);
+		/// Standard constructor
+		/** \param timeout in milliseconds
+		*/
+		Cooldown::Cooldown(int timeout);
+		BehaviorTree::BehaviorTreeList getChildren()
+		{
+			return BehaviorTree::BehaviorTreeList();
+		}
+	};
+	}
+
+}
+#endif
