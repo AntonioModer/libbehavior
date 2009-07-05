1a6f9e11a75bf7284c38c51d2c5eddbc1d37a72a
LeegleechN <LeegleechN@022deeea-31cc-11de-96ab-f16d9912eac9>
2009-07-05 17:28:32 +0000

    Documented existing examples

--- trunk/Snow Leopard/BehaviorTree/mainpage.h
+++ trunk/Snow Leopard/BehaviorTree/mainpage.h
@@ -1,7 +1,7 @@
 /**
 \mainpage Libbehavior Documentation
 
-@section Introduction to the library
+\section intro Introduction to the library
 
 This is the API Documentation for Libbehavior, a reactive AI library based on the concept of "behavior trees". A behavior tree is a way of organizing a collection of states and the decision processes for when to move between them. Behavior trees have many similiarities to Finite State Machines, but they have a number of additional features that make them more suitable for representing complex and potentially parallel behaviors.
 
@@ -9,31 +9,92 @@ Like Finite State Machines and unlike planners, Behavior Trees are reactive. Thi
 
 If this sounds useful to you, continue to the next section, where I explain exactly what behavior trees are and how they work.
 
-@section What is a Behavior Tree?
+\section whatis What is a Behavior Tree?
 
 A Behavior Tree, as one would guess, is a branching data structure that is used to represent behaviors. There are two types of nodes in a behavior tree: internal nodes and leaf nodes. Leaf nodes represent the most primitive actions that could be taken by a character. In a game, this could be walking in a particular direction, using a particular item, and doing a particular attack. The behavior of an agent can be defined as a sequence of these primitive actions, with the caveat that some of the actions could be happening simultaneously.
 
 Of course, a fixed linear sequence of actions is not a very interesting behavior. It lacks awareness of the situation and adaptivity. These qualities are given to the agent via the internal nodes. These nodes decide which of their children nodes to execute, using a number of different schemes. One internal node, the Parallel Node, executes all of its children simultaneously. By combining the Parallel Node with other nodes like the Priority or Probability Nodes, interesting behaviors can be developed that show awareness and adaptivity.
 
-@section What Internal Nodes are Available?
+\section internal What Internal Nodes are Available?
 
 This is a brief behavioral description of each of the supplied internal nodes. For specifics, please consult each node's documentation page.
 
-- Count Limit Node
+\subsection countlimit Count Limit Node
 This node restricts a particular path of behavior from executing more than the specified number of times. It can either put a strict limitation in repetition, or reset the limitation after some time. This is useful for, e.g. preventing your agents from repeating the same line of dialog to the point where the player will become annoyed.
-- ParallelNode
+\subsection parallel ParallelNode
 This is a powerful node that executes all of its children at the same time. This not just useful for simultanous behaviors, it can also be used for pursuing a main path of behavior while simultanously monitoring the world for problems that would make the behavior invalid. Examples of this type of usage are given in the test bench.
-- Priority Node
+\subsection priority Priority Node
 The Priority Node maintains an ordered sequence of behaviors, where earlier ones are more desireable (but may fail to complete), and later ones are less desireable, but presumably more likely to work. For example, if a character is trying to get through a door and they have the key in pocket, they can simply take out the key and open the door. If they don't have a key in pocket, that behavior will fail. The next behavior in priority order may be to go to the backyard and get the spare key. If that fails, the next behavior may be to call someone who has a key. This will continue until either one of the approaches succeeds, in which case the priority node will return success, or all of the nodes fail. In the latter case, the priority node will also return failure.
--Probability Node
+\subsection probability Probability Node
 The probability node introduces randomness into the agent's behavior. It works by associating a weight with each of its children. When it needs to select a behavior, it makes a random choice among its children, based on their weights. The weighting approach facilitates "tweaking" behaviors by allowing changes to be made without requiring that all the probabilities be re-balanced.
-- Repeat Node
+\subsection repeat Repeat Node
 The Repeat Node is unusual because it only has one child. It runs that child either a fixed number of times or indefinitely.
-- Sequential Node
+\subsection sequential Sequential Node
 The Sequential Node is the workhorse internal node. It simply runs all of its children in sequential order. If one of them fails, it also fails. Once all of them succeeds, it also succeeds.
 
 By composing these internal nodes together or possibly making your own, a large range of behavior can be expressed in a compact and reusable way.
 
-@section What Leaf Nodes are Available?
+\section leaf What Leaf Nodes are Available?
 
 Since leaf nodes are application-specific, you will have to implement most of them yourself. However, there are a handful of useful nodes provided. Some of them can wrap function and member calls in order to save you time and reduce code duplication. Others are handy "stand ins" for when some behavior is not yet implemented, or when you are debugging behaviors.
+\subsection always AlwaysRunning, AlwaysSuccess, AlwaysFailure
+These are the simplest leaf nodes. They simply return the indicated status value.
+\subsection after SuccessAfter, FailureAfter
+These have the 'running' status for a given number of iterations, then they return the indicated status.
+\subsection condition BoolCondition, FloatCondition, IntCondition
+These wrap function calls (with no arguments) that return the specified type. The BoolCondition can check whether the return value is a given one. The FloatCondition and IntCondition have a full set of comparisons (==,<,<=,>,>=).
+
+\section Behavior Tree Examples
+
+Included with the library are several examples of behavior trees in action. They are implemented in the context a "shoot-em-up" game, but of course the principles they use are applicable to anything. This is a short description of each of the behavior examples. For more information, consult the relevant "ScenarioX.h" header file. Before doing that, try actually playing the scenario to understand the behavior it implements.
+
+\section scenario1 Scenario 1
+
+This scenario implements to following behavior:
+
+-# Try to line up a shot on the player.
+-# If you have a shot lined up, fire, and then wait 500ms for the weapon to "recharge".
+
+This is the code that creates the behavior:
+
+\code
+opponent->brain
+	->addChild((new RepeatNode(-1))
+		->addChild((new SequentialNode())
+			->addChild(new BoolCondition<GameObject>(&GameObject::alignedWithPlayer,true))
+			->addChild(new Fire())
+			->addChild(new Cooldown(500))))
+	->addChild(new TurnTowardsTarget(.5));
+\endcode 
+
+Note that the "brain" node is a parallel node by default. The first indented section implements the firing behavior. The firing sequence is composed of
+
+-# A check to make sure there is a shot lined up.
+-# Actually firing at the player.
+-# A 500ms cooldown period.
+
+That firing sequence is wrapped in a RepeatNode, because the enemy should fire continuously at the player. If the RepeatNode was not there, the enemy would only fire once at the player, since after it fired once, that branch of the parallel node would have been considered a "success".
+
+The second parallel node branch demonstrates that leaf nodes do not have to be the simplest behaviors. "Turn towards a target" is a single unit of behavior, but the implementation is not as simple as, say, turn left or right. It would have been possible to implement TurnTowardsTarget using a behavior tree with "turn left" and "turn right" as leaves, but the benefits of doing so are questionable.
+
+Note the indentation style used for this section of code. This style of "chaining" function calls is the preferred way of creating behavior trees, because it concisely expresses the structure of the code.
+
+\section scenario2 Scenario 2
+
+This scenario demonstrates how behaviors can be reused within a project. The scenario implements an enemy that fires projectiles that "home in" on the player. However, this enemy has a special feature. If its health is below a critical threshold, the enemy will attempt a suicidal ramming attack on the player. The switch between the "firing" and "ramming" behavior is controlled with a priority node. The behavior tree used for the ramming attack is exactly the same as the behavior tree controlling the behavior of the projectiles. This shows that even if two objects have different types, as long as the behavior tree is written to the appropriate level of abstraction, it can apply across types.
+
+In this case, the behavior reuse is done by using a function that returns the appropriate Node. This approach could potentially be extended in more complex projects to use function arguments to customize the resulting behavior, lookup tables, or other features.
+
+\section scenario3 Scenario 3
+
+Scenario 3 demonstrates a more complex behavior, and also shows how to incorporate randomness into behavior. The behavior is as follows:
+
+-# Keep lined up with the player, and fire if aligned.
+-# Occasionally switch between firing positions, to keep the player confused.
+
+The "occasional" aspect of the position switch is controlled by a probability node. The firing behavior has a weight of 5.0, and the switching behavior has a weight of 1.0. Therefore, the enemy will decide switch positions about 1/6 of the times it is considering what to do.
+
+\section scenario4 Scenario 4
+
+Scenario 4 is another variant of an evasive enemy. This time, instead of switching positions randomly, the enemy bases its movement on the positions of the player and of incoming projectiles. If there are incoming projectiles nearby, the enemy will try to dodge them by moving away in a random direction. Otherwise, if the player has a shot lined up, the enemy will move out of the player's lane of fire. 
+*/
